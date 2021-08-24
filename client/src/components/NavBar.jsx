b3af40fef785c61bfb635ba8b6ff6e14b209ab1b
nghiavt2906 <nghiavt2906@gmail.com>
2021-08-24 11:59:22 +0700

    add routes

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -10,7 +10,9 @@ function NavBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">About</Nav.Link>
+            <Nav.Link href="/optimize">Price optimization</Nav.Link>
+            <Nav.Link href="/analysis">Data analysis</Nav.Link>
+            <Nav.Link href="/help">Help</Nav.Link>
           </Nav>
           <Nav></Nav>
         </Navbar.Collapse>
