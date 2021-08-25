787111dfe66a87bb148e487cb6f0c84ff278ec20
nghiavt2906 <nghiavt2906@gmail.com>
2021-08-25 16:22:08 +0700

    implement registration functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,8 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import User from "../models/user.js";
+
 class AuthController {
 	static async login(req, res) {
 
@@ -8,12 +13,20 @@ class AuthController {
 		const { username, password } = req.body
 
 		try {
+			const userInDb = await User.findOne({ username })
+			if (userInDb) return res.status(400).send('User already exists')
 
+			const passwordHash = await bcrypt.hash(password, 12)
+
+			const result = await User.create({ username, passwordHash })
+
+			const token = await jwt.sign({ username, id: result._id }, process.env.jwtSecret, { expiresIn: '5h' })
+
+			res.status(200).send(token)
 		} catch (error) {
-
+			console.log(`Error: ${error}`)
+			res.status(404).send('Something went wrong.')
 		}
-
-		return res.statusCode(200)
 	}
 }
 
