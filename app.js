8b4053867b91a34476bb67c21cb60f082dd2f9b4
nghiavt2906 <nghiavt2906@gmail.com>
2021-08-25 16:07:46 +0700

    add db connection and auth route

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
 import express from "express";
+
+import authRoute from "./routes/auth.js";
+
+dotenv.config()
 
 const app = express()
 
-const port = process.env.PORT || 5000
+app.use(express.json({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
-app.listen(port, () => console.log(`running on port ${port}`))
+app.use('/api/auth', authRoute)
+
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+	.then(() => {
+		console.log('connected to db.')
+
+		const port = process.env.PORT || 5000
+		app.listen(port, () => console.log(`running on port ${port}`))
+	})
+	.catch(e => console.log(`Error: ${e}`))
---
+++ controllers/auth.controller.js
@@ -0,0 +1,13 @@
+class AuthController {
+	static async login(req, res) {
+
+		return res.statusCode(200)
+	}
+
+	static async register(req, res) {
+
+		return res.statusCode(200)
+	}
+}
+
+export default AuthController
