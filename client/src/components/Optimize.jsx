d7dd57d935ec8590311d197d0469b60b87cb78d5
nghiavt2906 <nghiavt2906@gmail.com>
2021-09-09 16:16:23 +0700

    update price suggestion form

--- client/src/components/Optimize.jsx
+++ client/src/components/Optimize.jsx
@@ -12,7 +12,19 @@ function Optimize() {
           <Form.Control type="text" placeholder="Product name" />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="Category">
+        <Form.Group className="mb-3" controlId="brand">
+          <Form.Label>Brand name</Form.Label>
+          <Form.Select aria-label="Floating label select example">
+            <option>none brand</option>
+            <option value="1">Razer</option>
+            <option value="2">Nike</option>
+            <option value="3">Adidas</option>
+            <option value="4">OEM</option>
+            <option value="5">Sony</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="category">
           <Form.Label>Category name</Form.Label>
           <Form.Select aria-label="Floating label select example">
             <option>none category</option>
@@ -36,6 +48,25 @@ function Optimize() {
             <option value="4">Good</option>
             <option value="5">Very good</option>
           </Form.Select>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="shipping">
+          <Form.Label>Shipping method</Form.Label>
+          <Form.Select aria-label="Floating label select example">
+            <option>none shipping</option>
+            <option value="1">Post delivery</option>
+            <option value="2">Airline</option>
+            <option value="3">Sea</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="description">
+          <Form.Label>Product description</Form.Label>
+          <Form.Control
+            as="textarea"
+            placeholder="Product description"
+            style={{ height: '100px' }}
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
