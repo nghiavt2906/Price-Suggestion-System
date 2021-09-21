5c64a0db2ba634e85683268ff047f8b92354351b
nghiavt2906 <nghiavt2906@gmail.com>
2021-09-21 16:36:48 +0700

    test predicting price

--- predict.py
+++ predict.py
@@ -1,6 +1,7 @@
 import joblib
 import pandas as pd
 import numpy as np
+from keras.models import load_model
 
 def preprocess(df: pd.DataFrame) -> pd.DataFrame:
     df['name'] = df['name'].fillna('') + ' ' + df['brand_name'].fillna('')
@@ -18,10 +19,26 @@ def main():
 	}
 
 	encoder = joblib.load('./AI models/encoder.h5')
+	scaler = joblib.load('./AI models/scaler.h5')
+	model_1 = load_model('./AI models/model_1.h5')
+	model_b_1 = load_model('./AI models/model_b_1.h5')
+	model_2 = load_model('./AI models/model_2.h5')
+	model_b_2 = load_model('./AI models/model_b_2.h5')
 
 	df = pd.DataFrame(input)
 	X = encoder.transform(preprocess(df)).astype(np.float32)
-	print(X)
+	Xb = X.astype(np.bool).astype(np.float32)
+
+	pred_1 = model_1.predict(X)
+	pred_2 = model_2.predict(X)
+	pred_3 = model_b_1.predict(Xb)
+	pred_4 = model_b_2.predict(Xb)
+
+	pred = np.mean([pred_1, pred_2, pred_3, pred_4], axis=0)
+	pred = np.expm1(scaler.inverse_transform(pred.reshape(-1, 1))[:, 0])
+
+	print(pred)
+
 
 if __name__ == '__main__':
 	main()
