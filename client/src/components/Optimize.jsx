ac022c2dce30e7e1737f32f1d1f9608aea934353
nghiavt2906 <nghiavt2906@gmail.com>
2021-10-17 15:06:02 +0700

    add import data table

--- client/src/components/Optimize.jsx
+++ client/src/components/Optimize.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Form, Button, Spinner } from 'react-bootstrap';
+import { Container, Form, Button, Spinner, Table } from 'react-bootstrap';
 import axios from 'axios';
 
 function Optimize() {
@@ -40,94 +40,32 @@ function Optimize() {
   return (
     <Container className="mt-3 px-3 py-3">
       <h3>Price suggestion panel</h3>
-      {price == 0 ? (
-        <span>Please upload product infos</span>
-      ) : (
-        <span>Suggested price: {price}$</span>
-      )}
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="name">
-          <Form.Label>Product name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Product name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="brand">
-          <Form.Label>Brand name</Form.Label>
-          <Form.Select onChange={(e) => setBrand(e.target.value)}>
-            <option>none brand</option>
-            <option value="Razer">Razer</option>
-            <option value="Nike">Nike</option>
-            <option value="Adidas">Adidas</option>
-            <option value="OEM">OEM</option>
-            <option value="Sony">Sony</option>
-          </Form.Select>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="category">
-          <Form.Label>Category name</Form.Label>
-          <Form.Select onChange={(e) => setCategory(e.target.value)}>
-            <option>none category</option>
-            <option value="Men/Tops/T-shirts">Men/Tops/T-shirts</option>
-            <option value="2">
-              Electronics/Computers & Tablets/Components & Parts
-            </option>
-            <option value="3">Women/Tops & Blouses/Blouse</option>
-            <option value="4">Home/Home Décor/Home Décor Accents</option>
-            <option value="5">Women/Jewelry/Necklaces</option>
-          </Form.Select>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="productCondition">
-          <Form.Label>Product condition</Form.Label>
-          <Form.Select onChange={(e) => setCondition(e.target.value)}>
-            <option>none condition</option>
-            <option value="1">Very bad</option>
-            <option value="2">Bad</option>
-            <option value="3">New</option>
-            <option value="4">Good</option>
-            <option value="5">Very good</option>
-          </Form.Select>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="shipping">
-          <Form.Label>Shipping method</Form.Label>
-          <Form.Select onChange={(e) => setShipping(e.target.value)}>
-            <option>select shipping type</option>
-            <option value="0">Free (paid by seller)</option>
-            <option value="1">Paid</option>
-          </Form.Select>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="description">
-          <Form.Label>Product description</Form.Label>
-          <Form.Control
-            as="textarea"
-            placeholder="Product description"
-            style={{ height: '100px' }}
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </Form.Group>
-
-        <Button variant="primary" type="submit" disabled={isLoading}>
-          {isLoading && (
-            <Spinner
-              as="span"
-              animation="border"
-              size="sm"
-              role="status"
-              aria-hidden="true"
-              className="mr-2"
-            />
-          )}
-          {isLoading ? ' Loading...' : 'Submit'}
-        </Button>
-      </Form>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>Product name</th>
+            <th>Category name</th>
+            <th>Brand</th>
+            <th>Condition</th>
+            <th>Shipping</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </Table>
     </Container>
   );
 }
