2b719a8ff3529a322168fcc78249485ceacbb1d2
nghiavt2906 <nghiavt2906@gmail.com>
2021-10-21 18:40:54 +0700

    add similarity table to product details

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Table } from 'react-bootstrap';
+import getProductConditionLabel from '../utils/getProductConditionLabel';
+import getShippingLabel from '../utils/getShippingLabel';
 
 function Details({ show, handleClose, data }) {
   console.log(data);
   return (
-    <Modal show={show} onHide={handleClose} size="xl">
+    <Modal show={show} onHide={handleClose} dialogClassName="modal-90w">
       <Modal.Header closeButton>
         <Modal.Title style={{ color: 'black' }}>Product Details</Modal.Title>
       </Modal.Header>
@@ -13,7 +15,7 @@ function Details({ show, handleClose, data }) {
           <>
             <h4>{data.name}</h4>
             <span>
-              <b>Category:</b> {data.category_name}
+              <b>Category:</b> {data.category_name ?? 'empty'}
             </span>{' '}
             <br />
             <span>
@@ -21,11 +23,12 @@ function Details({ show, handleClose, data }) {
             </span>{' '}
             <br />
             <span>
-              <b>Condition:</b> {data.item_condition_id}
+              <b>Condition:</b>{' '}
+              {getProductConditionLabel(data.item_condition_id)}
             </span>{' '}
             <br />
             <span>
-              <b>Shipping:</b> {data.shipping}
+              <b>Shipping:</b> {getShippingLabel(data.shipping)}
             </span>{' '}
             <br />
             <span>
@@ -33,6 +36,36 @@ function Details({ show, handleClose, data }) {
             </span>
           </>
         )}
+
+        <Table striped bordered hover variant="dark" className="mt-3">
+          <thead>
+            <tr>
+              <th>Product name</th>
+              <th>Category name</th>
+              <th>Brand</th>
+              <th>Condition</th>
+              <th>Shipping</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Similarity</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Floral Kimono</td>
+              <td>Women/Sweaters/Cardigan</td>
+              <td>Vintage</td>
+              <td>{getProductConditionLabel(4)}</td>
+              <td>{getShippingLabel(1)}</td>
+              <td>
+                floral kimono -never worn -lightweight and perfect for hot
+                weather
+              </td>
+              <td>9.463</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </Table>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="danger" onClick={handleClose}>
