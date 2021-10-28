7653133a830989373d8943abb7860dd996e6942d
nghiavt2906 <nghiavt2906@gmail.com>
2021-10-28 23:29:28 +0700

    request cosine similarity service

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Modal, Table } from 'react-bootstrap';
 import getProductConditionLabel from '../utils/getProductConditionLabel';
 import getShippingLabel from '../utils/getShippingLabel';
 import ProgressBar from '@ramonak/react-progress-bar';
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
 function Details({ show, handleClose, data }) {
   const rgbToHex = (r, g, b) =>
@@ -14,6 +16,31 @@ function Details({ show, handleClose, data }) {
       ? [2 * 255 - redAndGreen, 255, 0]
       : [255, redAndGreen, 0];
   };
+
+  const [cosineRows, setCosineRows] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const main = async () => {
+      if (!data) return;
+
+      data.brand_name = data.brand_name ?? '';
+      data.item_description = data.item_description ?? '';
+
+      const res = await axios.post(
+        `${process.env.REACT_APP_COSINE_SIMILARITY_SERVICE_URL}/compare`,
+        data,
+        { validateStatus: false }
+      );
+
+      setCosineRows(res.data);
+      setIsLoading(false);
+    };
+
+    main();
+  }, [data]);
 
   return (
     <Modal show={show} onHide={handleClose} dialogClassName="modal-90w">
@@ -29,7 +56,10 @@ function Details({ show, handleClose, data }) {
             </span>{' '}
             <br />
             <span>
-              <b>Brand:</b> {data.brand_name ?? 'empty'}
+              <b>Brand:</b>{' '}
+              {data.brand_name === undefined || data.brand_name === ''
+                ? 'empty'
+                : data.brand_name}
             </span>{' '}
             <br />
             <span>
@@ -59,7 +89,19 @@ function Details({ show, handleClose, data }) {
           <h4>Similar Products</h4>
         </div>
 
-        <Table striped bordered hover variant="dark" className="mt-3">
+        <Table
+          striped
+          bordered
+          hover
+          variant="dark"
+          className="mt-3"
+          style={{
+            overflow: 'auto',
+            display: 'block',
+            height: isLoading ? '0' : '32rem',
+            width: '100%',
+          }}
+        >
           <thead>
             <tr>
               <th>Product name</th>
@@ -69,137 +111,48 @@ function Details({ show, handleClose, data }) {
               <th>Shipping</th>
               <th>Description</th>
               <th>Price</th>
-              <th>Similarity</th>
+              <th>Similarity (%)</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Bubble Mailers/Labels/Stickers</td>
-              <td>Other/Office supplies/Shipping Supplies</td>
-              <td>empty</td>
-              <td>{getProductConditionLabel(5)}</td>
-              <td>{getShippingLabel(1)}</td>
-              <td>
-                "20 - 4.5X8 Bubble Mailers (usable space 4.5X7) (Pink) 20 -
-                Shipping labels 25 - 1X3 Fragile stickers ➕➕Available to
-                add-on, just comment below:➕➕ Pic 2 - Shipping Labels 20 for
-                [rm] Pic 3 - Thank you please rate stickers 10/[rm] or 30/[rm]
-                Pic 4 - 1X3 Fragile Stickers 25 for [rm] **Ships next business
-                day, or sooner Monday - Saturday (except holidays) - Lined with
-                3/16"" bubble wrap - Approved by all carriers: USPS, UPS, FedEX,
-                DHL - Self Sealing - Water & Tear Resistant - CUSTOM LISTINGS
-                AVAILABLE @MrsCoolBreeze Price is FIRMs"
-              </td>
-              <td style={{ color: 'green' }}>
-                <b>$9.36</b>
-              </td>
-              <td style={{ width: '12%' }}>
-                <ProgressBar
-                  completed={86}
-                  bgColor={rgbToHex(...percentToRGB(86))}
-                  labelAlignment="outside"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>29 - 6.5X10 Bubble Mailers</td>
-              <td>Other/Office supplies/Shipping Supplies</td>
-              <td>empty</td>
-              <td>{getProductConditionLabel(4)}</td>
-              <td>{getShippingLabel(1)}</td>
-              <td>
-                29 - 6.5X10 Bubble Mailers (any colors are ok'd by the buyer)
-              </td>
-              <td style={{ color: 'green' }}>
-                <b>$12.0</b>
-              </td>
-              <td style={{ width: '12%' }}>
-                <ProgressBar
-                  completed={79}
-                  bgColor={rgbToHex(...percentToRGB(79))}
-                  labelAlignment="outside"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>35 Poly Mailers 1</td>
-              <td>Other/Office supplies/Shipping Supplies</td>
-              <td>empty</td>
-              <td>{getProductConditionLabel(4)}</td>
-              <td>{getShippingLabel(1)}</td>
-              <td>
-                "SUPERIOR QUALITY Very Strong & Durable Tear, Water & Dirt
-                Resistant (35) Pink Roses Self Sealing Poly Mailers NOT Bubble
-                Padded Polymailers Envelopes for Shipping 6"" x 9"" Size 100%
-                Recyclable 2.5mil Thickness Stretchable To Allow Overstuffing!
-                Very Lightweight - GREAT For Shipping! Great for Clothing Items
-                Can Be Used For Make Up, Video Games, DVDs & Other Small Items
-                WITH The Use Of Bubble Wrap!!! BRAND NEW FAST FREE SHIPPING
-                USPS, UPS, FedEx & DHL Approved!!! For Any 6"" x 9"" Size: 30 =
-                [rm] 35 = [rm] 45 = [rm] 50 = [rm] 100 = [rm] 200 = [rm] Search
-                NirmWorx and use the refine and filter tools to browse all our
-                GREAT DEALS! We Ship Orders Within 24 Hours Except Sundays -
-                Same Day Shipping On Orders Placed By 2PM EST! Price is Firm
-                PLEASE NO Low Ballers - We Do Not Respond We Offer Deals on
-                Bundles Please Contact Us First! We Take Great Pride in
-                Providing QUALITY Products and The Most EXCELLENT Service!
-                Please Check Our Ratings & Revie
-              </td>
-              <td style={{ color: 'green' }}>
-                <b>$6.0</b>
-              </td>
-              <td style={{ width: '12%' }}>
-                <ProgressBar
-                  completed={72}
-                  bgColor={rgbToHex(...percentToRGB(72))}
-                  labelAlignment="outside"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>Custom labels / for Mona</td>
-              <td>Other/Office supplies/Shipping Supplies</td>
-              <td>Anvie</td>
-              <td>{getProductConditionLabel(4)}</td>
-              <td>{getShippingLabel(0)}</td>
-              <td>
-                Reorder It's a girl sticker 30 labels Thank you labels 60 labels
-                For party favor
-              </td>
-              <td style={{ color: 'green' }}>
-                <b>$8.0</b>
-              </td>
-              <td style={{ width: '12%' }}>
-                <ProgressBar
-                  completed={48}
-                  bgColor={rgbToHex(...percentToRGB(48))}
-                  labelAlignment="outside"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>Floral Kimono</td>
-              <td>Women/Sweaters/Cardigan</td>
-              <td>Vintage</td>
-              <td>{getProductConditionLabel(4)}</td>
-              <td>{getShippingLabel(1)}</td>
-              <td>
-                floral kimono -never worn -lightweight and perfect for hot
-                weather
-              </td>
-              <td style={{ color: 'green' }}>
-                <b>$789.463</b>
-              </td>
-              <td style={{ width: '12%' }}>
-                <ProgressBar
-                  completed={24}
-                  bgColor={rgbToHex(...percentToRGB(24))}
-                  labelAlignment="outside"
-                />
-              </td>
-            </tr>
+            {isLoading
+              ? null
+              : cosineRows.map((row) => (
+                  <tr key={uuidv4()}>
+                    <td>{row.name}</td>
+                    <td>{row.category_name ?? 'empty'}</td>
+                    <td>{row.brand_name ?? 'empty'}</td>
+                    <td>{getProductConditionLabel(row.item_condition_id)}</td>
+                    <td>{getShippingLabel(row.shipping)}</td>
+                    <td>{row.item_description ?? 'empty'}</td>
+                    <td style={{ color: 'green' }}>
+                      <b>{'$' + row.price}</b>
+                    </td>
+                    <td style={{ width: '12%' }}>
+                      <ProgressBar
+                        completed={(row.similarity * 100).toFixed(2)}
+                        bgColor={rgbToHex(
+                          ...percentToRGB((row.similarity * 100).toFixed(2))
+                        )}
+                        labelAlignment="outside"
+                      />
+                    </td>
+                  </tr>
+                ))}
           </tbody>
         </Table>
+        {isLoading ? (
+          <img
+            src="https://www.icegif.com/wp-content/uploads/loading-icegif-1.gif"
+            style={{
+              width: '50%',
+              display: 'block',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+            }}
+            alt="Loading..."
+          />
+        ) : null}
       </Modal.Body>
       <Modal.Footer>
         <Button variant="danger" onClick={handleClose}>
--- similarity.py
+++ similarity.py
@@ -1,14 +1,13 @@
-import sys, json
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-lines = sys.stdin.read()
-lines = json.loads(lines)
-
 corpus = [
-	'who are you?'
+	'who are you?',
+	"what's up",
+	'hello'
 ]
-query = lines[0]
+
+query = 'hello'
 
 tfidf_vectorizer = TfidfVectorizer()
 tfidf_matrix_train = tfidf_vectorizer.fit_transform(corpus)
@@ -17,4 +16,4 @@ tfidf_query = tfidf_vectorizer.transform([query])
 
 similarity = cosine_similarity(tfidf_matrix_train, tfidf_query)
 
-print(similarity.max())
+print(similarity)
